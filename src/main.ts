fffdf723c949c8a12ce8c00ab6341bf735314622
EsraaElnajjar <114526210+EsraaElnajjar@users.noreply.github.com>
2025-04-24 18:24:32 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as express from 'express';
 import { join } from 'path';
 
-async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+const server = express();
 
-  // السماح بالوصول إلى مجلد 'uploads' عبر HTTP
+async function createApp(expressInstance: express.Express) {
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
-    prefix: '/uploads', // يجعل الصور متاحة عبر 'http://localhost:3000/uploads'
+    prefix: '/uploads',
   });
 
-  app.enableCors(); // السماح للفرونت إند بالوصول
-
-  await app.listen(3000);
-  console.log('🚀 Server running on http://localhost:3000');
+  app.enableCors();
+  await app.init();
 }
-bootstrap();
+
+createApp(server);
+
+export const handler = server;
